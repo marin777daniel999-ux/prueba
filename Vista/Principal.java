c33edbb6af628d676864164fd405df98ee750c7e
dλniel <marin777daniel999@gmail.com>
2026-04-25 17:53:00 -0500

    p2

--- Vista/Principal.java
+++ Vista/Principal.java
@@ -20,7 +20,7 @@ public class Principal {
     public static void menu() {
 
         System.out.println("\n1. Abrir banco");
-        System.out.println("2. Crear cuenta nueva");
+        System.out.println("2. Crear cuenta nueva otrraaaa");
         System.out.println("3. Consultar cuenta");
         System.out.println("4. Eliminar cuenta");
         System.out.println("5. Listar cuentas");
