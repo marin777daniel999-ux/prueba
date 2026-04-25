4164ad17659f03838f6095e6901e5889d6752c17
daniel <marin777daniel999@gmail.com>
2026-04-25 18:28:57 -0500

    a2

--- Vista/Principal.java
+++ Vista/Principal.java
@@ -22,7 +22,7 @@ public class Principal {
         System.out.println("\n1. Abrir banco");
         System.out.println("2. Crear cuenta nueva otrraaaa");
         System.out.println("3. Consultar cuenta vieja");
-        System.out.println("4. Eliminar cuenta ya");
+        System.out.println("4. Eliminar cuenta ya y ya");
         System.out.println("5. Listar cuentas");
         System.out.println("6. Transacciones");
         System.out.println("7. Salir hola lololol");
