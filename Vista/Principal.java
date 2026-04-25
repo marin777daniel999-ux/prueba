bcd5c21a0491578482dc248678c30675707bb8fd
daniel <marin777daniel999@gmail.com>
2026-04-25 18:25:25 -0500

    p6

--- Vista/Principal.java
+++ Vista/Principal.java
@@ -25,7 +25,7 @@ public class Principal {
         System.out.println("4. Eliminar cuenta");
         System.out.println("5. Listar cuentas");
         System.out.println("6. Transacciones");
-        System.out.println("7. Salir hola lol");
+        System.out.println("7. Salir hola lololol");
 
         String op = sc.next();
 
